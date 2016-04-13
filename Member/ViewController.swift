917d99ac324d3faca8c36332a2bc8695d53ad19c
蘇健豪 <jenhausu@icloud.com>
2016-04-13 10:59:55 +0800

    init project

---
+++ Member/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Member
+//
+//  Created by 蘇健豪1 on 2016/4/13.
+//  Copyright © 2016年 生蠔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
