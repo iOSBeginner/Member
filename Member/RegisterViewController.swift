37aa3724478583f663f84784b6a525eec763348a
蘇健豪 <jenhausu@icloud.com>
2016-04-13 14:58:41 +0800

    init RegisterViewController.swift

---
+++ Member/RegisterViewController.swift
@@ -0,0 +1,40 @@
+//
+//  RegisterViewController.swift
+//  Member
+//
+//  Created by 蘇健豪1 on 2016/4/13.
+//  Copyright © 2016年 生蠔. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewontroller: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func register(sender: UIButton) {
+        self.navigationController?.popViewControllerAnimated(true)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
